fd987a19dfcede8f682ad5b458dbc98caeb927f4
MchREsse <mail@randolphesser.de>
2022-12-21 00:17:44 +0100

    Update version string
    Version string updated by latest build number in version.h

--- include/version.h
+++ include/version.h
@@ -3,14 +3,14 @@
   #define VMAJOR "2"
 #endif
 #ifndef VMINOR
-  #define VMINOR "1"
+  #define VMINOR "0"
 #endif
 #ifndef BUILD_NUMBER
-  #define VBUILD "276"
+  #define VBUILD "278"
 #endif
 #ifndef VERSION
-  #define VERSION "v3.1.276 - 2022-04-17 14:38:39.864523"
+  #define VERSION "v3.0.278 - 2022-12-21 00:15:18.165428"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v3.1.276"
+  #define VERSION_SHORT "v3.0.278"
 #endif
